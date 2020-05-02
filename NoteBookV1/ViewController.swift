4d28fb27fa489f01179b0dfb3c05679b7a27779f
QL31 <qinglian.li1070@gmail.com>
2020-05-02 21:32:41 +0200

    Initial Commit

---
+++ NoteBookV1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NoteBookV1
+//
+//  Created by li qinglian on 02/05/2020.
+//  Copyright © 2020 li qinglian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
